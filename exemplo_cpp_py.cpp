306c0499494f53c639c7e8dcaad7a01905710829
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2021-06-10 16:28:42 -0300

    Cria uma funcao que pega os itens da amazon

--- exemplo_cpp_py.cpp
+++ exemplo_cpp_py.cpp
@@ -19,8 +19,48 @@ int main (int argc, char const *argv[]) {
 
 	Py_Initialize();
 
-	PyRun_SimpleString ("print('Hello World')\n"
-			    "print('O Jogo')");
+	PyRun_SimpleString (	"from bs4 import BeautifulSoup\n"
+				"import requests\n"
+				"from urllib.request import urlopen as uReq\n"
+				"import time\n"
+
+				"def getUrl (searchTerm):\n"
+				"	template = 'https://www.amazon.com.br/s?k={}'\n"
+				"	searchTerm = searchTerm.replace(' ', '+')\n"
+				"	return template.format(searchTerm)\n"
+
+				"def getItem (item):\n"
+				"	foi = False\n"
+
+				"	while (foi == False):\n"
+				"		url = getUrl(item)\n"
+				"		print (url)\n"
+				"		print ('Aguarde...')\n"
+				"		load = False\n"
+				"		html_text = requests.get(url).text\n"
+				"		soup = BeautifulSoup (html_text, 'lxml')\n"
+				"		products = soup.find_all ('div', class_ = 'sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col sg-col-4-of-20')\n"
+				"		print (type(products))\n"
+				"		if (products != []):\n"
+				"			foi = True\n"
+
+				"	for product in products:\n"
+				"		name = product.find('span', class_ = 'a-size-base-plus a-color-base a-text-normal').text\n"
+				"		price = True\n"
+
+				"		try:\n"
+				"			priceWhole = product.find ('span', class_ = 'a-price-whole').text\n"
+				"			priceFraction = product.find ('span', class_ = 'a-price-fraction').text\n"
+				"		except:\n"
+				"			price = False\n"
+
+				"		if price == True:\n"
+				"			print (name)\n"
+				"			print (priceWhole + priceFraction)\n"
+				"			print ()\n"
+
+				"getItem ('Iphone X')\n"
+				"print ('OK')");
 
 	if (Py_FinalizeEx() < 0) {
 		exit(120);
