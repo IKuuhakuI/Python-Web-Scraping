d9fa896929e874ceeb13110f14dfac6f71b5c299
Duda <mariamafra@poli.ufrj.br>
2021-06-11 03:38:09 -0300

    Primeiro commit aquivos da classe Store

---
+++ store.cpp
@@ -0,0 +1,29 @@
+/*
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e Computacao
+ * EEL670 - Linguagens de Programacao - Turma 2020/2
+ * Prof. Miguel Elias M. Campista
+ * Autor: Luiz Carlos Aguiar Carrion & Maria Eduarda de Souza Mafra
+ * Decricao: Implementacao das funcoes relacionadas a struct Store
+*/ 
+
+#include "store.h"
+
+Store::Store(string storeName, bool status) {
+	name =  storeName;
+	selected = status;
+}
+
+bool Store::isSelected() {
+	return selected;
+}
+
+void Store::changeSelection() {
+	if(selected == true)
+		selected = false;
+	else
+		selected = true;
+}
+
+
---
+++ store.h
@@ -0,0 +1,32 @@
+/*
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e Computacao
+ * EEL670 - Linguagens de Programacao - Turma 2020/2
+ * Prof. Miguel Elias M. Campista
+ * Autor: Luiz Carlos Aguiar Carrion & Maria Eduarda de Souza Mafra
+ * Trabalho Final - Etapa 2
+ * Decricao: Prototipo da struct Store
+*/ 
+
+#ifndef STORE_H
+#define STORE_H
+
+#include <string>
+
+using namespace std;
+
+class Store {
+	public:
+		Store(string, bool = true);
+
+		bool isSelected();
+		void changeSelection(); 	
+		string name;
+
+	private:
+		bool selected;
+};
+
+#endif
+
