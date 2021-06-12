1c5c26ff4b5d27c438040ec78233acdaabd2e55b
Luiz Carlos <luizcarlosaguiarc@gmail.com>
2021-06-12 17:01:28 -0300

    Cria arquivos com funcoes de manipulacao de arquivos

---
+++ files.h
@@ -0,0 +1,23 @@
+/*
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e Computacao
+ * EEL670 - Linguagens de Programacao - Turma 2020/2
+ * Prof. Miguel Elias M. Campista
+ * Autor: Luiz Carlos Aguiar Carrion & Maria Eduarda de Souza Mafra
+ * Trabalho Final - Etapa 2
+ * Decricao:
+ *
+*/ 
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "product.h"
+
+using namespace std;
+
+void fileWrite (string, vector <Product*>);
+void fileRead (string, vector <Product*>);
